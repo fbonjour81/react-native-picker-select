5d9016863140e435c9f1a3851279266ec5ab4991
Fabian Bonjour <fbonjour81@gmail.com>
2018-05-11 14:37:16 -0300

    .

--- src/index.js
+++ src/index.js
@@ -108,6 +108,7 @@ export default class RNPickerSelect extends PureComponent {
     renderPickerItems() {
 		console.log('RNPickerSelect-renderPickerItems');
         return this.state.items.map((item) => {
+			console.log(`RNPickerSelect-renderPickerItem-label:${label}-key:${key}-value:${value}`);
             return (
                 <Picker.Item label={item.label} value={item.value} key={item.key || item.label} />
             );
